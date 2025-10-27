347f4554ae5c4eb17067d928e4ddaa783ddc8d15
Your Name <you@example.com>
2025-10-27 16:57:31 +0530

    two push bswkw wifihwfohcw

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,11 +5,9 @@ import Footer from "./Footer";
 import ScrollToTop from "react-scroll-to-top";
 import { ArrowUp } from "lucide-react";
 const AnnouncementBanner = React.lazy(() => import("./AnnouncmentBanner"));
-
 const Layout = () => {
   const location = useLocation();
   const [isBannerVisible, setIsBannerVisible] = useState(true);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
@@ -17,30 +15,26 @@ const Layout = () => {
   const handleBannerVisibilityChange = (isVisible) => {
     setIsBannerVisible(isVisible);
   };
-
   return (
     <>
       <div className="fixed top-0 left-0 right-0 z-50">
         <React.Suspense fallback={<div className="h-8 secondaryColor" />}>
-          <AnnouncementBanner onVisibilityChange={handleBannerVisibilityChange} />
+          <AnnouncementBanner
+            onVisibilityChange={handleBannerVisibilityChange}
+          />
         </React.Suspense>
         <Header />
       </div>
-      <div
-        className={`${
-          isBannerVisible ? "pt-[50px]" : "pt-[9px]"
-        } layout-content`}
-      >
+      <div className={`${isBannerVisible ? "pt-[50px]" : "pt-[9px]"}`}>
         <Outlet />
+        <ScrollToTop
+          className="scrollToTop"
+          smooth
+          component={<ArrowUp size={20} className="upArrow" />}
+        />
+        <Footer />
       </div>
-      <Footer />
-      <ScrollToTop
-        className="scrollToTop"
-        smooth
-        component={<ArrowUp size={20} className="upArrow" />}
-      />
     </>
   );
 };
-
 export default Layout;
